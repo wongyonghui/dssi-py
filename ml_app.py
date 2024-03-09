90a6d11f65a51322065e7e73632a6b43c81a5201
wongyonghui <162765260+wongyonghui@users.noreply.github.com>
2024-03-09 11:28:00 +0800

    Add files via upload

---
+++ ml_app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# Generate some example data
+np.random.seed(0)
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+# Train the linear regression model
+model = LinearRegression()
+model.fit(X, y)
+
+# Streamlit UI
+st.title('Simple Linear Regression Model')
+
+st.write('Enter a value for X to predict y:')
+x_value = st.number_input('X')
+
+# Predict function
+def predict(x):
+    return model.predict([[x]])[0][0]
+
+if st.button('Predict'):
+    y_pred = predict(x_value)
+    st.write(f'Predicted y value: {y_pred:.2f}')
